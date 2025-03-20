afcbb74149ff56894bf9b1e62c49b602b524d3ee
lior-le <lior0076@gmail.com>
2025-03-20 22:51:05 +0200

    minor client modification

--- client/chatastrope_client.py
+++ client/chatastrope_client.py
@@ -18,20 +18,12 @@ async def get_scenarios():
                 return []
 
 
-def get_args(scenarios):
+def get_args():
     parser = argparse.ArgumentParser(description="Automatic testing for your AI chatbot")
     parser.add_argument("--url", type=str, help="Chatbot URL")
-    parser.add_argument("--filename", type=str, choices=scenarios, help="path of the file the report will be saved to") 
-    parser.add_argument("--scenarios", nargs="*", choices=scenarios, help="scenarios to test") # TODO handle errors with scenarios
+    parser.add_argument("--filename", type=str, help="path of the file the report will be saved to") 
+    parser.add_argument("--scenarios", nargs="*", help="scenarios to test") 
     return parser.parse_args()
-
-async def send_request(url, data):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.post(url, json=data) as response:
-                return await response.json()
-        except Exception as e:
-            return None
 
 def interactive_input(scenarios):
     console.print("[bold cyan]Interactive Mode: Enter Chatbot Details[/bold cyan]")
@@ -40,7 +32,6 @@ def interactive_input(scenarios):
     if not filename:
         filename = DEFAULT_FILENAME
     console.print("\n[bold yellow]Available Attack Scenarios:[/bold yellow]") # TODO change colors
-    scenarios = get_scenarios()
     for i, scenario in enumerate(scenarios, 1):
         console.print(f"{i}. {scenario}")
     inptut_scenarios = Prompt.ask("Select attack scenarios (comma-separated numbers, or 0 for all)", default=0)
@@ -54,16 +45,23 @@ def interactive_input(scenarios):
     return url, selected_scenarios, filename
 
 async def run_scenarios(url, scenarios):
-    response = await send_request(API_BASE_URL, {"url": url, "scenario": scenarios}) #TODO format based on the API response
-    if response:
-        return response
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(API_BASE_URL, json={"url": url, "scenario": scenarios}) as response:
+                status = await response.status()
+                if status == 200:
+                    return await response.read()
+                else: # TODO handle errors with request
+                    return None
+        except Exception as e:
+            return None
 
 async def main():
+    args = get_args()
     available_scenarios = get_scenarios()
-    args = get_args(available_scenarios)
     
     if args.url and args.scenarios and args.filename:
-        url, scenarios, filename = args.url, args.scenarios, args.filename
+        url, scenarios, filename = args.url, args.scenarios, args.filename # TODO handle errors with scenarios
     elif args.url and args.scenarios:
         url, scenarios, filename = args.url, args.scenarios, DEFAULT_FILENAME
     elif args.url:
@@ -74,7 +72,8 @@ async def main():
     console.print(f"\n[bold green]Testing your chatbot at {url}")
     
     async with open(filename, "wb") as f: # TODO handle errors with file   
-        f.write(await run_scenarios(url, scenarios))
+        data = await run_scenarios(url, scenarios)
+        f.write(data)
 
     console.print(f"\n[bold green]saved report to {filename}")
 
