b17bef63185cd56317f91b4aa79a2d4cd19a3852
Ofek <ofekstern6@gmail.com>
2025-05-04 21:20:08 +0300

    change

--- enums/owasp_llm.py
+++ enums/owasp_llm.py
@@ -6,12 +6,12 @@ from typing import Optional
 class OwaspLLM(Enum):
     PROMPT_INJECTION = "LLM01:2025 Prompt Injection"
     SENSITIVE_INFORMATION_DISCLOSURE = "LLM02:2025 Sensitive Information Disclosure"
-    SUPPLY_CHAIN = "LLM03:2025 Supply Chain"
-    DATA_AND_MODEL_POISONING = "LLM04:2025 Data and Model Poisoning"
+    #SUPPLY_CHAIN = "LLM03:2025 Supply Chain"
+    #DATA_AND_MODEL_POISONING = "LLM04:2025 Data and Model Poisoning"
     IMPROPER_OUTPUT_HANDLING = "LLM05:2025 Improper Output Handling"
-    EXCESSIVE_AGENCY = "LLM06:2025 Excessive Agency"
-    SYSTEM_PROMPT_LEAKAGE = "LLM07:2025 System Prompt Leakage"
-    VECTOR_AND_EMBEDDING_WEAKNESSES = "LLM08:2025 Vector and Embedding Weaknesses"
+    #EXCESSIVE_AGENCY = "LLM06:2025 Excessive Agency"
+    #SYSTEM_PROMPT_LEAKAGE = "LLM07:2025 System Prompt Leakage"
+    #VECTOR_AND_EMBEDDING_WEAKNESSES = "LLM08:2025 Vector and Embedding Weaknesses"
     MISINFORMATION = "LLM09:2025 Misinformation"
     UNBOUNDED_CONSUMPTION = "LLM10:2025 Unbounded Consumption"
 
